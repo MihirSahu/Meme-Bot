ab857b8e3ddaeac2a813dd509e1ca55b29617ef1
Mihir Sahu <2002mihir@gmail.com>
2021-09-28 11:21:03 -0500

    added misc reactions

--- MemeBot.py
+++ MemeBot.py
@@ -19,46 +19,46 @@ async def on_message(message):
 #        await message.add_reaction("\U0001F921")
 #    await client.process_commands(message)
 
-#@client.event
-#async def on_message(message):
-#    if "June" in message.content or "june" in message.content:
-#        await message.add_reaction('\U0001F63B')
-#    if "Simp" in message.content or "simp" in message.content:
-#        await message.add_reaction('\U0001F921')
-#    if "Hot" in message.content or "hot" in message.content:
-#        await message.add_reaction('\U0001F60B')
-#    if "Feet" in message.content or "feet" in message.content:
-#        await message.add_reaction('\U0001F4A6')
-#    if "LMAO" in message.content or "lmao" in message.content:
-#        await message.add_reaction('\U0001F602')
-#    if "LMFAO" in message.content or "lmfao" in message.content:
-#        await message.add_reaction('\U0001F923')
-#    if "no homo" in message.content:
-#        await message.add_reaction('\U0001F46C')
-#    if "thiCC" in message.content or "thick" in message.content or "thicc" in message.content:
-#        await message.add_reaction('\U0001F351')
-#    if "I-" in message.content or "lolol" in message.content:
-#        await message.add_reaction('\U0001F1FE')
-#        await message.add_reaction('\U0001F1EA')
-#        await message.add_reaction('\U0001F1F9')
-#        await message.add_reaction('\U0001F1EE')
-#    if "sp00k" in message.content or "spook" in message.content:
-#        await message.add_reaction('\U0001F631')
-#    if "thonk" in message.content:
-#        await message.add_reaction('\U0001F914')
-#    if "each other" in message.content:
-#        await message.add_reaction('\U0001F440')
-#    if "Bye" in message.content or "bye" in message.content:
-#        await message.add_reaction('\U0001F44B')
-#    if "gn" in message.content or "Gn" in message.content:
-#        await message.add_reaction('\U0001F319')
-#    if "shit" in message.content:
-#        await message.add_reaction('\U0001F4A9')
-#    if "rip" in message.content or "Rip" in message.content:
-#        await message.add_reaction('\U0001F622')
-#    if "oof" in message.content:
-#        await message.add_reaction('\U0001F921')
-#    await client.process_commands(message)
+@client.event
+async def on_message(message):
+    if "June" in message.content or "june" in message.content:
+        await message.add_reaction('\U0001F63B')
+    if "Simp" in message.content or "simp" in message.content:
+        await message.add_reaction('\U0001F921')
+    if "Hot" in message.content or "hot" in message.content:
+        await message.add_reaction('\U0001F60B')
+    if "Feet" in message.content or "feet" in message.content:
+        await message.add_reaction('\U0001F4A6')
+    if "LMAO" in message.content or "lmao" in message.content:
+        await message.add_reaction('\U0001F602')
+    if "LMFAO" in message.content or "lmfao" in message.content:
+        await message.add_reaction('\U0001F923')
+    if "no homo" in message.content:
+        await message.add_reaction('\U0001F46C')
+    if "thiCC" in message.content or "thick" in message.content or "thicc" in message.content:
+        await message.add_reaction('\U0001F351')
+    if "I-" in message.content or "lolol" in message.content:
+        await message.add_reaction('\U0001F1FE')
+        await message.add_reaction('\U0001F1EA')
+        await message.add_reaction('\U0001F1F9')
+        await message.add_reaction('\U0001F1EE')
+    if "sp00k" in message.content or "spook" in message.content:
+        await message.add_reaction('\U0001F631')
+    if "thonk" in message.content:
+        await message.add_reaction('\U0001F914')
+    if "each other" in message.content:
+        await message.add_reaction('\U0001F440')
+    if "Bye" in message.content or "bye" in message.content:
+        await message.add_reaction('\U0001F44B')
+    if "gn" in message.content or "Gn" in message.content:
+        await message.add_reaction('\U0001F319')
+    if "shit" in message.content:
+        await message.add_reaction('\U0001F4A9')
+    if "rip" in message.content or "Rip" in message.content:
+        await message.add_reaction('\U0001F622')
+    if "oof" in message.content:
+        await message.add_reaction('\U0001F921')
+    await client.process_commands(message)
 
 @client.command()
 async def yomomma(ctx):
